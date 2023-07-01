4abc56819a02d0e6a4e68eb295afe75772e4bcf4
Rakshith Joseph Manaloor <92179379+RakshithJoseph@users.noreply.github.com>
2023-07-01 16:46:41 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
-      "mongodb+srv://RakshithJ:rchat_2002@rchat.lndqofz.mongodb.net/?retryWrites=true&w=majority",
+      "*****************",
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
